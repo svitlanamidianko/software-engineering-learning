80115233e1aafb3d376dc8025d17310460814aa0
Zane Sand <frankzanesand@gmail.com>
2020-01-27 10:12:04 -0600

    Updated utils/personal_exercises.py with better abstraction

--- utils/personal_exercises.py
+++ utils/personal_exercises.py
@@ -1,5 +1,11 @@
 import hashlib
 
+sessions = {'3.2': ['2-Logging', 
+                    '3-Graphics'], 
+            '9.1': ['2-The Current Weather', 
+                    '3-Crypto Exchange', 
+                    '4-Google directions', 
+                    '5-Flying Aircraft']}
 
 def problem_choice(email, seminar, problems):
     email = email.strip().lower()
@@ -10,13 +16,7 @@ def problem_choice(email, seminar, problems):
     ind = int(md5, 16) % len(problems)
     return problems[ind]
 
-
 if __name__ == '__main__':
-    email = input('Please enter your student email address:')
-    print('For seminar 3.2:')
-    print(problem_choice(email, '3.2', ['2-Logging', '3-Graphics']))
-    print(" ")
-    print('For seminar 9.1:')
-    print(problem_choice(email, '6.2',
-                         ['2-The Current Weather', '3-Crypto Exchange',
-                          '4-Google directions', '5-Flying Aircraft']))
+    email = input('Please enter your student email address: ')
+    sessions_key = input('What session is this for (sessions supported: {}): '.format(', '.join([*sessions])))
+    print(problem_choice(email, sessions_key, sessions[sessions_key]))
